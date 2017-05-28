bedaea2002044a68ec8f5a7febde38ddd18a7dae
savva.volobuev <savva.volobuev@gmail.com>
2017-05-28 12:44:29 +0300

    [*] Camera permissions

--- app/src/main/java/com/brandcash/CaptureQrActivity.java
+++ app/src/main/java/com/brandcash/CaptureQrActivity.java
@@ -1,11 +1,16 @@
 package com.brandcash;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.zxing.ResultPoint;
 import com.google.zxing.client.android.BeepManager;
@@ -24,6 +29,7 @@ public class CaptureQrActivity extends Activity {
     private DecoratedBarcodeView barcodeView;
     private BeepManager beepManager;
     private String lastText;
+    private static final int CAMERA_PERM = 32;
 
     private BarcodeCallback callback = new BarcodeCallback() {
         @Override
@@ -58,6 +64,46 @@ public class CaptureQrActivity extends Activity {
         barcodeView.decodeContinuous(callback);
 
         beepManager = new BeepManager(this);
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_PERM);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERM: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(this, "we can't scan", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
     @Override
