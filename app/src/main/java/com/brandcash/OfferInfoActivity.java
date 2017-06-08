a944cc6e42cfcfb61fcbb576350ff1d1c71d8327
savva.volobuev <savva.volobuev@mretailer.ru>
2017-06-08 17:42:29 +0300

    [*] navigation and offer info

--- app/src/main/java/com/brandcash/OfferInfoActivity.java
+++ app/src/main/java/com/brandcash/OfferInfoActivity.java
@@ -1,9 +1,11 @@
 package com.brandcash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,8 +13,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.brandcash.model.Offer;
+import com.brandcash.ui.PriceView;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by savva.volobuev on 28.05.2017.
@@ -21,6 +28,11 @@ import com.brandcash.model.Offer;
 public class OfferInfoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView navigationView;
     private Offer offer;
+    private TextView brandDescription;
+    private TextView brandUrl;
+    private TextView offerDescription;
+    private PriceView priceView;
+    private ImageView brandImage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,16 +42,42 @@ public class OfferInfoActivity extends AppCompatActivity implements NavigationVi
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.offer);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState();*/
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         offer = getIntent().getParcelableExtra(OfferListActivity.EXTRA_OFFER);
+
+        brandDescription = (TextView) findViewById(R.id.brand_description);
+        brandImage = (ImageView) findViewById(R.id.image);
+        brandUrl = (TextView) findViewById(R.id.brand_url);
+        offerDescription = (TextView) findViewById(R.id.description);
+        priceView = (PriceView) findViewById(R.id.price);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        brandDescription.setText(offer.getBrand().getBrandDescription());
+        brandUrl.setText(offer.getBrand().getBrandSiteUrl());
+        offerDescription.setText(offer.getDescription());
+        priceView.setPrice(offer.getBudget());
+        Picasso.with(BrandcashApp.getAppContext())
+                .load(offer.getBrand().getBrandImageUrl())
+                .into(brandImage);
     }
 
     @Override
@@ -50,7 +88,11 @@ public class OfferInfoActivity extends AppCompatActivity implements NavigationVi
         } else if (id == R.id.nav_cash) {
 
         } else if (id == R.id.nav_offer) {
-
+            TaskStackBuilder.create(this)
+                    .addParentStack(MainActivity.class)
+                    .addNextIntent(new Intent(this, OfferListActivity.class))
+                    .startActivities();
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
